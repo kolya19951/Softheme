e7ba2041d728e39be6b44beea3999bab0414da1a
kolya19951 <kolya.simotyuk@gmail.com>
2015-10-12 18:27:47 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,23 @@
+package classes;
+
+/**
+ * Created by Коля on 12.10.2015.
+ */
+public class Main {
+    public static void main(String[] args) {
+        String text = "11111110110011111100001111111";
+        int tmp = 0;
+        int max = 0;
+        for (int i = 0; i < text.length(); i++) {
+            if (text.charAt(i) == '1') {
+                tmp++;
+                if (tmp > max)
+                    max = tmp;
+            }
+            else{
+                tmp = 0;
+            }
+        }
+        System.out.println(max);
+    }
+}
