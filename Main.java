7d0dc937e55b8662ba50887df79f4011d5870fce
kolya19951 <kolya.simotyuk@gmail.com>
2015-10-12 18:28:18 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,3 @@
-package classes;
-
-/**
- * Created by Коля on 12.10.2015.
- */
 public class Main {
     public static void main(String[] args) {
         String text = "11111110110011111100001111111";
